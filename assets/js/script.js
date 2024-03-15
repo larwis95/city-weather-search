bfd2693c638e916913ba40f85bcfdd95be090b4b
Larry Wisniewski <larryw48390@gmail.com>
2024-03-15 03:26:49 -0400

    finished forecast, and error handling logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,22 +31,22 @@ class api {
         this.url = url;
     }
  
-    async create() {
+    async call() {
         try {
             let response = await fetch(this.url);
  
             if (response.status === 404) {
                 if (this.type === 'forecast') {
-                } else if (this.type === 'today') {
+                } 
+                else if (this.type === 'today') {
                     alert(`${savedCity} not found for search`);
                     const errorCity = savedHistory.indexOf(savedCity)
-                    console.log(savedHistory);
-                    savedHistory.splice(errorCity, 1);
+                    savedHistory[0].splice(errorCity, 1);
+                    savedHistory[1].splice(errorCity, 1);
                     savedCity = savedHistory[savedHistory.length-1]
                     localStorage.setItem('cityHistory', JSON.stringify(savedHistory));
                     localStorage.setItem('lastCity', savedCity);
                     const firstLi = $('ul > li:first-child');
-                    console.log(`first li ${firstLi}`)
                     const searchBox = searchForm.find('input');
                     firstLi.remove();
                     searchBox.val('');
@@ -58,6 +58,8 @@ class api {
  
             switch (this.type) {
                 case 'forecast':
+                    console.log(data);
+                    renderForeCast(data);
                     // renderForeCast(data);
                     break;
                 case 'today':
@@ -80,42 +82,51 @@ function arraymove(arr, fromIndex, toIndex) {
  
 function initAutocomplete() {
     const searchInput = document.querySelector('#searchCity');
-    const geocoder = new google.maps.Geocoder();
     const options = {
             types: ['(cities)'],
             componentRestrictions: {country: "us"}
            };
-    console.log(searchInput);
     const autocomplete = new google.maps.places.Autocomplete(searchInput, options);
     
     autocomplete.addListener('place_changed', () => {
         const place = autocomplete.getPlace();
-        console.log(place);
-        console.log("autocomplete " + autocomplete)
         savedPlace = place;
-        console.log(place);
-        geocoder
-            .geocode({placeId: savedPlace.place_id})
-                .then(({results}) => {
-                    coords = [results[0].geometry.location.lat().toFixed(2), results[0].geometry.location.lng().toFixed(2)];
-                    console.log(coords);
-                    savedImg = savedPlace.photos[0].getUrl({maxWidth: 500, maxHeight: 500});
-                    
-                })
-                .catch((e) => window.alert("Geocoder failed due to: " + e));
-    })
+        const objLength = Object.keys(savedPlace).length
+        console.log(objLength);
+        if (objLength > 1) { 
+            savedImg = savedPlace.photos[0].getUrl();
+            localStorage.setItem('lastImg', savedImg);
+            getCoordinates(place);
+        } else {
+            return;
+        }
+        })
+}
 
+function getCoordinates(place) {
+    console.log(place);
+    coords = [place.geometry.location.lat().toFixed(2), place.geometry.location.lng().toFixed(2)];
 }
 
 function searchSubmit() {
     const searchInput = searchForm.find('input');
-    const cityName = searchInput.val().toLowerCase();
-    savedCity = cityName;
-    updateSearchHistory(cityName);
-    updateDropDown();
-    saveToLocalStorage();
-    searchInput.val('');
-    setUrls();
+    console.log(savedPlace.length)
+    const objLength = Object.keys(savedPlace).length
+    if (objLength > 1) { 
+        const cityName = searchInput.val().toLowerCase();
+        savedCity = cityName;
+        localStorage.setItem('lastCoords', JSON.stringify(coords));
+        updateSearchHistory(cityName);
+        updateDropDown();
+        saveToLocalStorage();
+        searchInput.val('');
+        setUrls();
+    }
+    else {
+        alert('Invalid Search, use autocomplete for valid search');
+        searchInput.val('');
+    return;
+    }
 }
 
 function updateSearchHistory(cityName) {
@@ -150,78 +161,95 @@ function saveToLocalStorage() {
 }
 
 function setUrls() {
-    console.log(coords);
     todayUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&appid=${weatherKey}&units=imperial`;
     forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${coords[0]}&lon=${coords[1]}&appid=${weatherKey}&units=imperial`;
-    localStorage.setItem('lastCoords', JSON.stringify(coords));
     handleApiCall();
 }
 
 function handleApiCall() {
     const todayApi = new api('today', todayUrl);
     const forecastApi = new api('forecast', forecastUrl);
-    todayApi.create();
+    todayApi.call();
+    forecastApi.call();
 }
 
-function handleHistoryClick(id) {
-    const key = 'AIzaSyDAICu2uRtO8MB_8-vw03aONwaK-fVV5us'
-        fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${id}&key=${key}`)
-            .then((response) => {
-                if (response.status === 404) {
-                    alert('Place not found!');
-                    return;
-                }
-                return response.json();
-                }
-            )
-            .then ((results) => {
-                coords = [results[0].geometry.location.lat().toFixed(2), results[0].geometry.location.lng().toFixed(2)];
-                console.log(coords);
-                savedImg = savedPlace.photos[0].getUrl({maxWidth: 500, maxHeight: 500});
-                setUrls();
-            })
+function handleHistoryClick(id, tar) {
+    const place = new google.maps.places.PlacesService(tar);
+    place.getDetails({placeId: id}, (results, PlaceServiceStatus) => {
+        console.log(results);
+        savedImg = results.photos[0].getUrl();
+        savedCity = results.formatted_address;
+        getCoordinates(results);
+        setUrls();
+        updateDropDown();
+    })
 }
 
-function renderToday(data,) {
+function renderToday(data) {
     todayCont.empty();
-    console.log(data);
     const lowercaseCity = savedCity.toLowerCase();
     const city = lowercaseCity.toLowerCase().split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
     const weatherIcon = checkWeatherIcon(data);
     todayCont.append(`
-      <div class="card text-bg-dark" id="currentWeatherCard">
+      <div class="card text-bg-dark align-items-center" id="currentWeatherCard">
                   <img src="" class="card-img border border-white" alt="${city} skyline" id="cardImg">
-                <div class="card-img-overlay">
-                  <div class="card col-6 text-center">
-                    <h4 class="card-title" id="currentCity">${city} - Today</h4>
+                    <div class="card-img-overlay d-flex flex-column flex-wrap align-items-start justify-content-start align-content-center">
+                    <h4 class="card-title " id="currentCity">${city} - Today</h4>
                     <h5 class="card-text">${Math.round(data.main.temp)} °F ${weatherIcon}</h5>
                     <p class="card-text mb-0">Humidity: ${data.main.humidity}%</p>
                     <p class="card-text">Wind Speed: ${Math.round(data.wind.speed)} mph</p>
                 </div>
                 </div>
-              </div>
     `);
     const cityImg = $('#cardImg');
-    localStorage.setItem('lastImg', savedImg);
     cityImg.attr('src', savedImg);
 
 }
 
+function renderForeCast(data) {
+    const dayData = [data.list[10], data.list[18], data.list[26], data.list[34], data.list[39]];
+    forecastCont.empty();
+    console.log(dayData);
+    for (let i = 0; i < dayData.length; i++) {
+        let day = dayjs(dayData[i].dt_txt).format('MM/DD/YYYY');
+        let weatherIcon = checkWeatherIcon(dayData[i]);
+        forecastCont.append(`
+        <div class="col-2 card text-bg-dark mb-3 text-center" style="max-width: 18rem;">  
+        <div class="card-header">${day}</div>
+        <div class="card-body">
+          <h5 class="card-title">${Math.round(dayData[i].main.temp)} °F</h5>
+          <h5 class="card-title">${weatherIcon}</h5>
+          <p class="card-text">Humidity:</p>
+          <p class="card-text">${dayData[i].main.humidity}%</p>
+          <p class="card-text">Wind:</p> 
+          <p class="card=text">${Math.round(dayData[i].wind.speed)} mph</p>
+        </div>
+        </div>`);
+    }
+}
+
 function checkWeatherIcon(data) {
-    console.log(data.weather[0].main);
     switch (data.weather[0].main) {
-        case ('Rain' || 'Drizzle'):
+        case ('Drizzle'):
+        case ('Mist'):
+        case ('Rain'):
             return weatherTypes.rain;
+
         case ('Thunderstorm'):
             return weatherTypes.storm;
+
         case ('Snow'):
             return weatherTypes.snow;
+
         case ('Clouds'):
             return weatherTypes.someClouds;
+
         default:
             return weatherTypes.sunny;
     }
 }
+
+
 
 $(document).ready(() => {
     if (cityHistory !== null) {
@@ -239,11 +267,7 @@ $(document).ready(() => {
     }
     const places = savedHistory[1].slice().reverse();
     const citiesReversed = savedHistory[0].slice().reverse()
-    console.log(places[0])
     for (let i = 0; i < citiesReversed.length; i++) {
-        console.log(i)
-        console.log(savedHistory[1])
-        console.log(places[i])
         hisUl.append(`<li class="dropdown-item" data-placeid="${places[i]}">${citiesReversed[i]}</li>`);
     }
     if (hisUl.children().length === 0) {
@@ -258,8 +282,11 @@ $(document).ready(() => {
     hisUl.on('click', (event) => {
         event.stopPropagation();
         const tar = $(event.target);
-        const id = tar.attr('data-placeid')
-        handleHistoryClick(id);
+        if (tar.get(0).nodeName === 'LI') {
+        const id = tar.attr('data-placeid');
+        handleHistoryClick(id, event.target);
+        }
+    return;
     })
 })
 
